f5732592dea561ed71001df4b23d9dd5dd0d6ee1
Xarus117 <cezarygebczyk117@gmail.com>
2022-03-25 09:01:21 +0100

    Barra de acciones, botón de guardar y de salir
    25 de marzo, he añadido una barra de acciones que cambia según el valor del contador de acciones, un botón para guardar (no funcional) y un botón para salir de la partida (volver la menú principal)

--- PandemicPt7ElMenuV2/src/panel/PanelNuevaPartida.java
+++ PandemicPt7ElMenuV2/src/panel/PanelNuevaPartida.java
@@ -17,7 +17,9 @@ import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 public class PanelNuevaPartida extends JPanel implements ActionListener {
 
@@ -31,6 +33,17 @@ public class PanelNuevaPartida extends JPanel implements ActionListener {
 	JButton vacunaVerde;
 	JButton vacunaGris;
 
+	JButton guardar;
+
+	JButton salir;
+
+	JButton accion1;
+	JButton accion2;
+	JButton accion3;
+	JButton accion4;
+
+	Image image;
+
 	// BOOLEAN PARA ACTIVAR O DESACTIVAR LAS VACUNAS
 	boolean azulb = false;
 	boolean amarillab = false;
@@ -38,7 +51,8 @@ public class PanelNuevaPartida extends JPanel implements ActionListener {
 	boolean verdeb = false;
 	boolean grisb = false;
 
-	Image image;
+	// ACCIONES
+	int contadorAccion = 4;
 
 	PanelNuevaPartida() {
 		setLayout(null);
@@ -113,8 +127,47 @@ public class PanelNuevaPartida extends JPanel implements ActionListener {
 			System.out.println("Ha ocurrido un error al mostrar el mapa");
 		}
 
-		// LAS PUTAS VACUNAS
-		
+		// ICONO GUARDAR
+
+		guardar = new JButton();
+		guardar.setIcon(null);
+
+		guardar.setSize(50, 50);
+		guardar.setLocation(30, 670);
+		guardar.setBackground(Color.red);
+		guardar.setBorder(null);
+
+		try {
+			guardar.setIcon(new ImageIcon(ImageIO.read(new File("Imagenes//guardar.png"))));
+		} catch (IOException e2) {
+			System.out.println("Ha ocurrido un error al cargar el botón de guardar el estado de la partida");
+		}
+
+		add(guardar);
+		guardar(); // FUNCIÓN PARA GUARDAR
+
+		// SALIR
+
+		salir = new JButton();
+
+		salir.setIcon(null);
+		salir.setSize(130, 40);
+		salir.setLocation(1130, 10);
+		salir.setBackground(Color.green);
+		salir.setBorder(null);
+
+		try {
+			salir.setIcon(new ImageIcon(ImageIO.read(new File("Imagenes//salir.png"))));
+		} catch (IOException e2) {
+			System.out.println("Ha ocurrido un error al cargar el botón de guardar el estado de la partida");
+		}
+
+		salir.addActionListener(this);
+
+		add(salir);
+
+		// INICIAR VACUNAS
+
 		vacunaVerde = new JButton();
 		vacunaAzul = new JButton();
 		vacunaAmarilla = new JButton();
@@ -125,48 +178,68 @@ public class PanelNuevaPartida extends JPanel implements ActionListener {
 		vacunaAmarilla.setIcon(null);
 		vacunaRoja.setIcon(null);
 
+		// INICIAR ACCIONES
+
+		accion1 = new JButton();
+		accion2 = new JButton();
+		accion3 = new JButton();
+		accion4 = new JButton();
+
+		accion1.setIcon(null);
+		accion2.setIcon(null);
+		accion3.setIcon(null);
+		accion4.setIcon(null);
+
+		acciones(contadorAccion);
+		vacunas();
+
+	}
+
+	public void vacunas() { // LAS PUTAS VACUNAS
+		vacunaAzul.setSize(100, 100);
+		vacunaAzul.setLocation(1070, 750);
+		vacunaAzul.setContentAreaFilled(false);
+		vacunaAzul.setBorder(null);
+
+		vacunaAmarilla.setSize(100, 100);
+		vacunaAmarilla.setLocation(1140, 750);
+		vacunaAmarilla.setContentAreaFilled(false);
+		vacunaAmarilla.setBorder(null);
+
+		vacunaRoja.setSize(100, 100);
+		vacunaRoja.setLocation(1000, 750);
+		vacunaRoja.setContentAreaFilled(false);
+		vacunaRoja.setBorder(null);
+
+		vacunaVerde.setSize(100, 100);
+		vacunaVerde.setLocation(930, 750);
+		vacunaVerde.setContentAreaFilled(false);
+		vacunaVerde.setBorder(null);
+
 		try {
-
 			if (azulb) {
-
 				vacunaAzul.setIcon(new ImageIcon(ImageIO.read(new File("Imagenes//vacunaAzul.png"))));
 			} else {
 				vacunaAzul.setIcon(new ImageIcon(ImageIO.read(new File("Imagenes//vacunaGris.png"))));
 			}
-			vacunaAzul.setSize(100, 100);
-			vacunaAzul.setLocation(1070, 750);
-			vacunaAzul.setContentAreaFilled(false);
-			vacunaAzul.setBorder(null);
 
 			if (amarillab) {
 				vacunaAmarilla.setIcon(new ImageIcon(ImageIO.read(new File("Imagenes//vacunaAmarilla.png"))));
 			} else {
 				vacunaAmarilla.setIcon(new ImageIcon(ImageIO.read(new File("Imagenes//vacunaGris.png"))));
 			}
-			vacunaAmarilla.setSize(100, 100);
-			vacunaAmarilla.setLocation(1140, 750);
-			vacunaAmarilla.setContentAreaFilled(false);
-			vacunaAmarilla.setBorder(null);
 
 			if (rojab) {
 				vacunaRoja.setIcon(new ImageIcon(ImageIO.read(new File("Imagenes//vacunaRoja.png"))));
 			} else {
 				vacunaRoja.setIcon(new ImageIcon(ImageIO.read(new File("Imagenes//vacunaGris.png"))));
 			}
-			vacunaRoja.setSize(100, 100);
-			vacunaRoja.setLocation(1000, 750);
-			vacunaRoja.setContentAreaFilled(false);
-			vacunaRoja.setBorder(null);
 
 			if (verdeb) {
 				vacunaVerde.setIcon(new ImageIcon(ImageIO.read(new File("Imagenes//vacunaVerde.png"))));
 			} else {
 				vacunaVerde.setIcon(new ImageIcon(ImageIO.read(new File("Imagenes//vacunaGris.png"))));
 			}
-			vacunaVerde.setSize(100, 100);
-			vacunaVerde.setLocation(930, 750);
-			vacunaVerde.setContentAreaFilled(false);
-			vacunaVerde.setBorder(null);
 
 		} catch (Exception e) {
 			System.out.println("Ha ocurrido un error al mostrar las vacunas");
@@ -183,6 +256,66 @@ public class PanelNuevaPartida extends JPanel implements ActionListener {
 		add(vacunaVerde);
 	}
 
+	public void acciones(int contadorAccion) { // LAS PUTAS ACCIONES
+		try {
+			accion1.setSize(20, 100);
+			accion1.setLocation(40, 750);
+			accion1.setContentAreaFilled(false);
+			accion1.setBorder(null);
+
+			accion2.setSize(20, 100);
+			accion2.setLocation(70, 750);
+			accion2.setContentAreaFilled(false);
+			accion2.setBorder(null);
+
+			accion3.setSize(20, 100);
+			accion3.setLocation(100, 750);
+			accion3.setContentAreaFilled(false);
+			accion3.setBorder(null);
+
+			accion4.setSize(20, 100);
+			accion4.setLocation(130, 750);
+			accion4.setContentAreaFilled(false);
+			accion4.setBorder(null);
+
+			if (contadorAccion == 4) {
+				accion1.setIcon(new ImageIcon(ImageIO.read(new File("Imagenes//Accion.png"))));
+				accion2.setIcon(new ImageIcon(ImageIO.read(new File("Imagenes//Accion.png"))));
+				accion3.setIcon(new ImageIcon(ImageIO.read(new File("Imagenes//Accion.png"))));
+				accion4.setIcon(new ImageIcon(ImageIO.read(new File("Imagenes//Accion.png"))));
+			}
+
+			if (contadorAccion <= 3) {
+				accion4.setIcon(null);
+				accion4.setIcon(new ImageIcon(ImageIO.read(new File("Imagenes//AccionNo.png"))));
+			}
+			if (contadorAccion <= 2) {
+				accion3.setIcon(null);
+				accion3.setIcon(new ImageIcon(ImageIO.read(new File("Imagenes//AccionNo.png"))));
+			}
+			if (contadorAccion <= 1) {
+				accion2.setIcon(null);
+				accion2.setIcon(new ImageIcon(ImageIO.read(new File("Imagenes//AccionNo.png"))));
+			}
+			if (contadorAccion <= 0) {
+				accion1.setIcon(new ImageIcon(ImageIO.read(new File("Imagenes//AccionNo.png"))));
+			}
+
+			add(accion1);
+			add(accion2);
+			add(accion3);
+			add(accion4);
+
+		} catch (Exception e) {
+			System.out.println("Ha ocurrido un error al mostrar las acciones");
+		}
+
+	}
+
+	public void guardar() {
+		// FUNCIÓN PARA GUARDAR LA PARTIDA CON FICHEROS
+	}
+
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
@@ -192,14 +325,28 @@ public class PanelNuevaPartida extends JPanel implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
-		if (e.getSource() == boton1) { 
-		System.out.println("Toni maricon");
+		if (e.getSource() == boton1) {
+			System.out.println(contadorAccion);
+			contadorAccion--;
+			acciones(contadorAccion);
 		} else if (e.getSource() == boton2) {
-		
-			// PRUEBAS CAMBIO DE COLOR | VACUNAS
-		
-		} else if (e.getSource() == vacunaAzul) {
-			azulb = true;
+			System.out.println(contadorAccion);
+			contadorAccion--;
+			acciones(contadorAccion);
+
+		}
+
+		else if (e.getSource() == salir) {
+			JFrame marco = (JFrame) SwingUtilities.getWindowAncestor(this);
+			marco.remove(this);
+			marco.add(new PanelPrincipal());
+			marco.setVisible(true);
+		} else if (e.getSource() == vacunaAzul) { // PRUEBAS CAMBIO DE COLOR | VACUNAS
+			if (azulb == false) {
+				azulb = true;
+			} else {
+				azulb = false;
+			}
 			try {
 				if (azulb) {
 
@@ -214,7 +361,11 @@ public class PanelNuevaPartida extends JPanel implements ActionListener {
 			add(vacunaAzul);
 
 		} else if (e.getSource() == vacunaAmarilla) {
-			amarillab = true;
+			if (amarillab == false) {
+				amarillab = true;
+			} else {
+				amarillab = false;
+			}
 			try {
 				if (amarillab) {
 					vacunaAmarilla.setIcon(new ImageIcon(ImageIO.read(new File("Imagenes//vacunaAmarilla.png"))));
@@ -227,7 +378,11 @@ public class PanelNuevaPartida extends JPanel implements ActionListener {
 			add(vacunaAmarilla);
 
 		} else if (e.getSource() == vacunaRoja) {
-			rojab = true;
+			if (rojab == false) {
+				rojab = true;
+			} else {
+				rojab = false;
+			}
 			try {
 				if (rojab) {
 					vacunaRoja.setIcon(new ImageIcon(ImageIO.read(new File("Imagenes//vacunaRoja.png"))));
@@ -240,7 +395,11 @@ public class PanelNuevaPartida extends JPanel implements ActionListener {
 			add(vacunaRoja);
 
 		} else if (e.getSource() == vacunaVerde) {
-			verdeb = true;
+			if (verdeb == false) {
+				verdeb = true;
+			} else {
+				verdeb = false;
+			}
 			try {
 				if (verdeb) {
 
