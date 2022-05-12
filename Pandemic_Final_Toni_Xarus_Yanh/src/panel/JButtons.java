3e816aee7b6e28e296e17a6fe3870fce22b99ab8
toniodin <tonichiqueroencinas@gmail.com>
2022-05-12 21:01:48 +0200

    12/05/2022
    Final del pandemic

---
+++ Pandemic_Final_Toni_Xarus_Yanh/src/panel/JButtons.java
@@ -0,0 +1,57 @@
+package panel;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
+
+
+public class JButtons extends JButton {
+		//<-- VARIABLE -->
+
+	private String nombre;
+
+	
+	//<-- CONSTRCTOR -->
+
+	public JButtons(String nombre, int x, int y) {
+		
+		//<-- VARIABLE -->
+		
+		Image im = Toolkit.getDefaultToolkit().createImage("imagenes//cursorDefecto.png");
+		Image im2 = Toolkit.getDefaultToolkit().createImage("imagenes//cursorHover.png");
+		Cursor cur = Toolkit.getDefaultToolkit().createCustomCursor(im, new Point(10, 10), "WILL");
+		Cursor cur2 = Toolkit.getDefaultToolkit().createCustomCursor(im2, new Point(10, 10), "WILL"); 
+		this.nombre = nombre;
+		this.setBounds(x - 90, y - 95, 15, 15);
+		this.setForeground(Color.WHITE);
+		this.setBorder(null);
+		this.setBorderPainted(false);
+		this.setContentAreaFilled(false);
+		this.setBorderPainted(false);
+		this.setVisible(true);
+		this.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				setCursor(cur2);
+			}
+
+			public void mouseExited(MouseEvent e) {
+				setCursor(cur);
+			}
+		});
+	}
+	
+	//<-- GET AND SET-->
+
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+};
