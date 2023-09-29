5dc15c4ca880b6cd5b3f3cc591540a3c19a2a21a
Cezary Gebczyk <74736356+Xarus117@users.noreply.github.com>
2023-09-29 14:40:49 +0200

    Cambios ficheros

---
+++ PANDEMIC (Juego por turnos)/src/panel/Marco.java
@@ -0,0 +1,52 @@
+package panel;
+
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.*;
+
+public class Marco extends JFrame {
+
+	PanelPrincipal MenuPrincipal;
+	Dimension tamanoPantalla;
+	static Clip clip;
+
+	//<-- CONSTRUCTOR -->
+	//<-- CREACION Y DEFINICION DE LA VENTANA -->
+	Marco() {
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Fin del programa al pulsar la X
+		setTitle("BIOHAZARD, PANDEMIC");
+		Image Logo = Toolkit.getDefaultToolkit().getImage("imagenes\\Logo.png");
+        setIconImage(Logo);
+		setResizable(false);//Pa k no se pueda maximizar la pantalla
+		this.tamanoPantalla = Toolkit.getDefaultToolkit().getScreenSize();
+		setSize(1280,920);
+		MenuPrincipal = new PanelPrincipal();
+		add(MenuPrincipal);
+		ReproducirSonido();
+		setVisible(true);// Modificar la visibilidad del marco
+	}
+	//<-- AUDIO -->
+	private void ReproducirSonido() {
+
+		try {
+			AudioInputStream audioInputStream = AudioSystem
+					.getAudioInputStream(new File("Audios\\Gwyn.wav").getAbsoluteFile());
+			clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.start();
+		} catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+			System.out.println("Error al reproducir el sonido.");
+		}
+		
+	}
+}
